dfe88a1a6aa5afb5ab54dca8b6dee1affdca7bc9
lbart21 <79618751+lbart21@users.noreply.github.com>
2022-02-03 13:07:23 +1000

    Add files via upload

---
+++ LukesExplicitStiffenedEOSAlgorithm/EOSElementCoefficients.py
@@ -0,0 +1,13 @@
+coefficients = {}
+coefficients["water"] = {
+    "p_inf" : 6.2178e8,
+    "b" : 6.7212e-4,
+    "q" : -1177788,
+    "gamma" : 1.19,
+    "Cp" : 4285,
+    "Cv" : 3610
+}
+coefficients["bad"] = {
+    "p_inf" : 8.5e8,
+    "gamma" : 2.8
+}
